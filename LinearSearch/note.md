a0c65adbac21b8515203c0998bae9fd080e1f3be
tyraelHqy <752546894@qq.com>
2020-08-06 02:22:47 +0800

    1. 测试算法性能

--- LinearSearch/note.md
+++ LinearSearch/note.md
@@ -1,4 +1,24 @@
+## 循环不变量
 ![循环不变量](https://raw.githubusercontent.com/tyraelHqy/cloudimg/master/data/20200806013638.png)
 在循环每一次开始的时候，所有的变量都满足的条件，这个条件被称为循环不变量。
 **循环体**所做的事情就是在**维持**这个**循环不变量**。
 ![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200806015451.png)
+
+## 复杂度分析
+复杂度分析：表示算法的性能
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200806020014.png)
+常数不重要；复杂度描述的是随着数据规模n的增大，算法性能的变化趋势
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200806020228.png)
+
+## 常见算法复杂度
+- 线性查找法 O(n)
+- 一个数组中的元素可以组成哪些数据对 O(n^2)
+- 数字n的二进制位数 
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200806020612.png)
+- 数字n的所有约数
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200806020820.png)
+- 长度为n的二进制数字,长度为n的数组的所有排列
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200806020945.png)
+- 判断数字n是否是偶数？ O(1)
+
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200806021052.png)
--- LinearSearch/src/LinearSearch.java
+++ LinearSearch/src/LinearSearch.java
@@ -15,17 +15,25 @@ public class LinearSearch {
     }
 
     public static void main(String[] args) {
-        Integer[] data = {24,18,12,17,16,66,32,4};
+        int[] dataSize = {1000000,10000000};
+        for(int n:dataSize) {
+            Integer[] data = ArrayGenerator.generatorOrderedArray(n);
 
-        int res = LinearSearch.search(data, 16);
-        System.out.println(res);
+            long start = System.nanoTime();
+            for (int i = 0; i < 100; i++) {
+                LinearSearch.search(data, n);
+            }
+            long end = System.nanoTime();
+            double time = (end - start) / 1000000000.0;
+            System.out.println("n = " + n + " , 100 runs: " + time + " s");
+        }
 
-        int res2 = LinearSearch.search(data, 666);
-        System.out.println(res2);
-
-        Student[] students = {new Student("Alice"),new Student("Leo")};
-        Student leo = new Student("Leo");
-        int res3 = LinearSearch.search(students,leo);
-        System.out.println(res3);
+//        int res2 = LinearSearch.search(data, 666);
+//        System.out.println(res2);
+//
+//        Student[] students = {new Student("Alice"),new Student("Leo")};
+//        Student leo = new Student("Leo");
+//        int res3 = LinearSearch.search(students,leo);
+//        System.out.println(res3);
     }
 }
