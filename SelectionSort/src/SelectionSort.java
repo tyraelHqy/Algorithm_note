e563aa7cc15898cf7967d368ac3983ce2a799972
tencent_tyraelhuang <wx_98c6e313a6c8447da1ef8be6b29896e4@git.code.tencent.com>
2020-08-06 20:33:17 +0800

    1. 实现选择排序算法 2. 使用带约束的泛型

--- SelectionSort/src/SelectionSort.java
+++ SelectionSort/src/SelectionSort.java
@@ -1,2 +1,33 @@
 public class SelectionSort {
+
+    private SelectionSort() {
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // 选择arr[i...n) 中最小值的索引
+            int minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
+                    minIndex = j;
+                }
+                swap(arr, i, minIndex);
+            }
+        }
+    }
+
+    private static <E> void swap(E[] arr, int i, int j) {
+        E temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 4, 3, 5, 6};
+        SelectionSort.sort(arr);
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
 }
