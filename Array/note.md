26aaadab7600af2aea31325817a32844789db364
tyraelHqy <752546894@qq.com>
2020-08-20 20:39:21 +0800

    1. 新建动态数组类，完成了动态数组的基本方法，成员方法成员变量以及简单的查询数组中的参数等等方法。

---
+++ Array/note.md
@@ -0,0 +1,22 @@
+# 数据结构
+## 数据结构基础
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200817180939.png)
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200817181008.png)
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200817181112.png)
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200817181222.png)
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200817181316.png)
+
+数据结构研究的是数据如何在计算机中进行组织和存储，使得我们可以高效的获取数据或者修改数据。
+
+在 **内存** 世界的 **增删改查** 。
+
+## 数组
+![](https://cdn.jsdelivr.net/gh/tyraelHqy/cloudimg@master/img/20200820193405.png)
+
+索引可以有语意；也可以没有语意;
+
+数组最大的优点：快速查询。scores[2]
+
+但并非所有有语意的索引都适用于数组;
+
+基于java的数组，二次封装属于我们自己的数组类
---
+++ Array/src/main/java/Array.java
@@ -0,0 +1,49 @@
+public class Array {
+
+    private int[] data;
+    private int size;
+
+    /**
+     * 构造函数，传入数组的容量capacity构造Array
+     *
+     * @param capacity
+     */
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    /**
+     * 无参的构造函数，默认数组的容量是Capacity=10
+     */
+    public Array() {
+        this(10);
+    }
+
+    /**
+     * 获取数组中的元素个数
+     *
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 获取数组的容量
+     *
+     * @return
+     */
+    public int getCapacity() {
+        return data.length;
+    }
+
+    /**
+     * 判断数组是不是为空
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+}
