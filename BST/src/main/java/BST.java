7c4f014ad4099a8b778ddcd023460df16139e989
tyraelHqy <752546894@qq.com>
2020-09-12 19:53:01 +0800

    1. 二分搜索树改进实现

--- BST/src/main/java/BST.java
+++ BST/src/main/java/BST.java
@@ -29,35 +29,25 @@ public class BST<E extends Comparable<E>> {
     }
 
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
-            size++;
-        } else {
-            add(root, e);
-        }
+        root = add(root, e);
     }
 
     /**
      * 向以node为根的二分搜索树中插入元素E，递归算法
+     *
      * @param node
      * @param e
      */
-    private void add(Node node, E e) {
-        if (e.equals(node.e)) {
-            return;
-        } else if (e.compareTo(node.e) < 0 && node.left == null) {
-            node.left = new Node(e);
+    private Node add(Node node, E e) {
+        if (node == null) {
             size++;
-            return;
-        } else if (e.compareTo(node.e) > 0 && node.right == null) {
-            node.right = new Node(e);
-            size++;
-            return;
+            return new Node(e);
         }
         if (e.compareTo(node.e) < 0) {
-            add(node.left, e);
-        } else {
-            add(node.right, e);
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) < 0) {
+            node.right = add(node.right, e);
         }
+        return node;
     }
 }
