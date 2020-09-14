72fca9e514dc346fd4eb8ea236af196b7b3543f9
tyraelHqy <752546894@qq.com>
2020-09-14 10:50:48 +0800

    1. 二分搜索树查询元素

--- BST/src/main/java/BST.java
+++ BST/src/main/java/BST.java
@@ -50,4 +50,28 @@ public class BST<E extends Comparable<E>> {
         }
         return node;
     }
+
+    /**
+     * 看二分搜索树种是否包含元素e
+     *
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, E e) {
+
+        if (node == null) {
+            return false;
+        }
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
 }
